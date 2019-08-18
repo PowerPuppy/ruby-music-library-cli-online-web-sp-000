95ddaecce1d3188de724e5211379890aad0ea36c
Chris Cheong <chrisxcheong@gmail.com>
2019-08-18 19:14:46 +0000

    Automatically backed up by Learn

--- lib/Artist.rb
+++ lib/Artist.rb
@@ -2,6 +2,20 @@ class Artist
 attr_accessor :name
 @@all = []
 
+def initialize(name)
+  @name = name
+end
 
+def self.all
+  @@all
+end
+
+def self.destroy_all
+  @@all = []
+end
+
+def save
+  @@all << self
+end
 
 end
